3d0079936ac47af6dc4c4d71748471028c927c77
Alison Peisker <apeisker@fnal.gov>
2024-08-21 10:47:29 -0500

    setting up for first tests

---
+++ tests/env.py
@@ -0,0 +1,40 @@
+import os
+import sys
+import pytest
+
+production=os.environ.get("DDISP_TEST_PRODUCTION", False)
+
+if not production:
+    base = os.path.dirname(os.path.dirname(__file__))
+    os.environ["PATH"] = f"{base}/data_dispatcher/ui:{os.environ['PATH']}"
+#    os.environ["PYTHONPATH"] = f"{base}:{base}/tests/mocks:{os.environ.get('PYTHONPATH','')}"
+    os.environ["PYTHONPATH"] = f"{base}/data_dispatcher:{os.environ['PYTHONPATH']}"
+    sys.path.insert(0,base)
+#    sys.path.insert(0,f"{base}/tests/mocks")
+
+@pytest.fixture
+def env():
+    if production:
+       hostaport = 'https://metacat.fnal.gov:8143'
+       hostport = 'https://metacat.fnal.gov:9443'
+    else:
+       hostaport = 'https://metacat.fnal.gov:8143'
+       hostport = 'http://fermicloud761.fnal.gov:9094'
+
+
+    os.environ['METACAT_AUTH_SERVER_URL'] = f'{hostaport}/auth/hypot_dev'
+    os.environ['DATA_DISPATCHER_URL'] = f'{hostport}/hypot_dd/data'
+    os.environ['METACAT_SERVER_URL'] = f'{hostport}/hypot_meta_dev/app'
+    os.environ['DATA_DISPATCHER_AUTH_URL'] = f'{hostaport}/auth/hypot_dev'
+    os.environ['BEARER_TOKEN_FILE'] = '/tmp/bt_mc_test%d' % os.getpid()
+    print("METACAT_SERVER_URL=", os.environ["METACAT_SERVER_URL"])
+    print("DATA_DISPATCHER_URL=", os.environ["DATA_DISPATCHER_URL"])
+
+@pytest.fixture
+def token(env):
+    os.system("htgettoken -i hypot -a htvaultprod.fnal.gov ")
+    
+@pytest.fixture
+def auth(token):
+    os.system("metacat auth login -m token $USER")
+    os.system("ddisp login -m token $USER")
--- tests/test_commandline.py
+++ tests/test_commandline.py
@@ -1,101 +1,114 @@
 import pytest
 import os
 
-#test_proj = 
+from env import env, token, auth
 
-def test_ddisp_help():
+test_proj = "files from mengel:gen_cfg"
+
+def test_ddisp_help(auth):
     with os.popen("ddisp help", "r") as fin:
         data = fin.read()
         assert data.find("login") > 0
         assert data.find("version") > 0
 
-def test_ddisp_version():
+def test_ddisp_version(auth):
     with os.popen("ddisp version", "r") as fin:
         data = fin.read()
         assert data.find("Server version") > 0
         assert data.find("Client version") > 0
 
-def test_ddisp_login_token():
+def test_ddisp_login_token(auth, token):
     with os.popen(f"ddisp login -m token {os.environ['USER']}", "r") as fin:
         data = fin.read()
         assert data.find(os.environ["USER"]) > 0
         assert data.find("User") >= 0
         assert data.find("Expires") >= 0
 
-def test_ddisp_project_create():
+@pytest.fixture
+def test_ddisp_project_create(auth):
     with os.popen(f"ddisp project create {test_proj} ", "r") as fin:
         data = fin.read()
         assert type(data) == int
+    return data
 
-def test_ddisp_project_copy():
-    with os.popen(f"ddisp project copy {proj_id} ", "r") as fin:
-        data = fin.read()
-        assert type(data) == int
-        assert data != proj_id
-
-def test_ddisp_project_show():
-    with os.popen(f"ddisp project show {proj_id}", "r") as fin:
+def test_ddisp_project_show(auth, test_ddisp_project_create):
+    with os.popen(f"ddisp project show {test_ddisp_project_create}", "r") as fin:
         data = fin.read()
     assert data.find("Owner") > 0
     assert data.find("Status") > 0
     assert data.find("Created") > 0
 
-def test_ddisp_project_list():
+#def test_ddisp_project_copy(auth, test_ddisp_project_create):
+#    with os.popen(f"ddisp project copy {test_ddisp_project_create} ", "r") as fin:
+#        data = fin.read()
+#        assert type(data) == int
+#        assert data != proj_id
+
+def test_ddisp_project_list(auth):
     with os.popen("ddisp project list", "r") as fin:
         data = fin.read()
     assert data.find("owner") > 0
     assert data.find("created") > 0
     assert data.find("state") > 0
 
-def test_ddisp_project_search():
+def test_ddisp_project_search(auth):
+    with os.popen("ddisp project search -s active", "r") as fin:
+        data = fin.read()
+    assert data.find(str(proj_id)) > 0
+    assert data.find(os.environ["USER"]) > 0
+
+#def test_ddisp_project_restart():
 
 
-def test_ddisp_project_restart():
+#def test_ddisp_project_cancel():
+#    os.system(f"ddisp project cancel {proj_id} ")
+#    with os.popen(f"ddisp project show {proj_id} ", "r") as fin:
+#        data = fin.read()
+#    assert data.find("cancelled") > 0
+
+#def test_ddisp_project_activate():
+#    os.system(f"ddisp project activate {proj_id} ")
+#    with os.popen(f"ddisp project show {proj_id} ", "r") as fin:
+#        data = fin.read()
+#    assert data.find("active") > 0
+
+#def test_ddisp_project_delete():
 
 
-def test_ddisp_project_activate():
+#def test_ddisp_file_show():
 
 
-def test_ddisp_project_cancel():
-
-
-def test_ddisp_project_delete():
-
-
-def test_ddisp_file_show():
-
-
-def test_ddisp_file_list():
+#def test_ddisp_file_list():
  
 
-def test_ddisp_worker_id():
+def test_ddisp_worker_id(auth):
     with os.popen("ddisp worker id", "r") as fin:
         data = fin.read()
     assert data > 0
 
-def test_ddisp_worker_list():
+#def test_ddisp_worker_list():
 
 
-def test_ddisp_worker_next():
+#def test_ddisp_worker_next():
 
 
-def test_ddisp_worker_done():
+#def test_ddisp_worker_done():
 
 
-def test_ddisp_worker_fail():
+#def test_ddisp_worker_fail():
 
 
-def test_ddisp_rse_list():
+def test_ddisp_rse_list(auth):
     with os.popen("ddisp rse list", "r") as fin:
         data = fin.read()
     assert data.find("Name") > 0
     assert data.find("Status") > 0
     assert data.find("Description") > 0
 
-def test_ddisp_rse_set():
+#def test_ddisp_rse_set():
 
 
-def test_ddisp_rse_show():
+def test_ddisp_rse_show(auth):
     with os.popen("ddisp rse show FNAL_DCACHE_DISK_TEST", "r") as fin:
         data = fin.read()
     assert data.find("RSE") > 0
