4d5967f866efbb8d45947d0c2c8508858bec1c0e
Alison Peisker <apeisker@fnal.gov>
2024-12-10 14:57:40 -0600

    upload_pypi fix

--- upload_pypi.sh
+++ upload_pypi.sh
@@ -1,5 +1,8 @@
-#!/bin/sh
+#!/bin/bash
+
 
 rm -rf build dist *.egg-info
-python setup.py sdist bdist_wheel
-twine upload dist/*
+#python setup.py sdist bdist_wheel
+python3 -m build
+twine upload --verbose  dist/*
+rm -rf build dist *.egg-info
